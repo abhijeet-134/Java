07f97b74737aa6a291254c15b2c54f1dd59f7b05
Abhijeet Taywade <abhijeettaywade14@gmail.com>
2026-07-31 21:23:35 +0530

    Even Numbers From 1 To N

---
+++ Assignment8.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+// Print Even Numbers From 1 To N
+public class Assignment7 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number :");
+        int n = sc.nextInt();
+        for(int i=2; i<=n; i++) {
+            if(i % 2 == 0) {
+                System.out.println(i + " is Even");
+            }
+        }
+    }
+}
+
