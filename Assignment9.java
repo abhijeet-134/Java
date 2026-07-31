70ac2bfe814623c9b75c334be573d858a67f8ae3
Abhijeet Taywade <abhijeettaywade14@gmail.com>
2026-07-31 21:30:51 +0530

    Odd Numbers From 1 to N

---
+++ Assignment9.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+// Print Odd Numbers From 1 To N
+public class Assignment7 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number :");
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++) {
+            if(i % 2 != 0) {
+                System.out.println(i + " is Odd ");
+            }
+        }
+    }
+}
