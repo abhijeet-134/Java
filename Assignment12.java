e60cbc2791034c273b1452ab40a9ab7f7437df06
Abhijeet Taywade <abhijeettaywade14@gmail.com>
2026-08-01 15:44:59 +0530

    Sum of all Digits of Given Number

--- Assignment12.java
+++ Assignment12.java
@@ -1,23 +1,6 @@
 import java.util.*;
 
 // Sum of all digits of given number..
-// public class Assignment12 {
-//     public static void main(String args[]) {
-//         Scanner sc = new Scanner(System.in);
-//         System.out.println("Enter a number:");
-//         int n = sc.nextInt();
-//         int sum = 0;
-//         while( n > 0) {
-//             int lastdigit = n % 10;
-//             sum = sum + lastdigit;
-//             n = n / 10;
-
-//         }
-//         System.out.println("Sum of given Numbers is :" + sum);
-
-//     }
-// }
-
 public class Assignment12 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
