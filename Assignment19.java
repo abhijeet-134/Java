1583054fc0e09faecb50d29d10771c53fefc471b
Abhijeet Taywade <abhijeettaywade14@gmail.com>
2026-08-02 21:47:56 +0530

    Prints Reverse Number 1 To N

---
+++ Assignment19.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+
+// Prints  Reverse numbers  from 1 to N 
+public class Assignment17 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter  Number :");
+        int n = sc.nextInt();
+        for(int i=n; i>=1; i--) {
+            System.out.println(i);
+        }
+
+    }
+}
+
+
+// public class Assignment17 {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter  Number :");
+//         int n = sc.nextInt();
+//         for(int i=1; i<=n; i++) {
+//             System.out.println(i);
+//         }
+
+//     }
+// }
