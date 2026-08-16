fd68db60ef4967ca2ddfb40ef776c506d3e7b1df
Abhijeet Taywade <abhijeettaywade14@gmail.com>
2026-08-16 12:54:58 +0530

    Prints Pairs in Array

--- Array5.java
+++ Array5.java
@@ -1,21 +1,7 @@
 import java.util.*;
 
-// public class Array5 {
-//     public static void pairsArray(int numbers[]) {
-//         for(int i=0; i<numbers.length; i++) {
-//             int curr = numbers[i];
-//             for(int j=i+1; j<numbers.length; j++) {
-//                 System.out.print("(" + curr + "," + numbers[j] + ")");
-//             }
-//             System.out.println();
-//         }
-//     } 
-//     public static void main(String args[]) {
-//         int numbers[] = {2, 4, 6, 8, 10};
-//         pairsArray(numbers);
-//     }
-// }
 
+// // Prints Pairs in Array
 public class Array5 {
     public static void pairsArray(int numbers[]) {
         for(int i=0; i<numbers.length; i++) {
