a0e5af8f4449c471f8c37f2075b9cc2fa863f92f
Abhijeet Taywade <abhijeettaywade14@gmail.com>
2026-08-23 15:50:26 +0530

    Add trapping rain water Solution using auxiliary arrays

--- Array11.java
+++ Array11.java
@@ -1,22 +1,24 @@
 import java.util.*;
 
-public  class Array11 {
-    public static int trappingRainWater(int height[]) {
-        // calculate leftMax Boundary 
+// // Trapping Rain water code (with if -else)
+public class Array11 {
+    public static int trappedRainwater(int height[]) {
+        // left Max boundary 
         int leftMax[] = new int[height.length];
 
         leftMax[0] = height[0];
         for(int i=1; i<height.length; i++) {
-            if(leftMax[i-1] >  height[i]) {
+            if(leftMax[i-1] > height[i]) {
                 leftMax[i] = leftMax[i-1];
             }else {
                 leftMax[i] = height[i];
             }
         }
 
-        // Calculate right Max boundary 
+        // // Right Max Boundary 
         int rightMax[] = new int[height.length];
         int last = height.length-1;
+
         rightMax[last] = height[last];
         for(int i=height.length-2; i>=0; i--) {
             if(rightMax[i+1] > height[i]) {
@@ -26,18 +28,37 @@ public  class Array11 {
             }
         }
 
-        int totalWater = 0;
+        // calculate trapped rain  water 
+        int trappedRainwater = 0;
+        int  waterLevel;
         for(int i=0; i<height.length; i++) {
-            int waterlevel = Math.min(leftMax[i], rightMax[i]);
-            int waterOnBar = waterlevel - height[i];
-            totalWater += waterOnBar;
+            if(leftMax[i] < rightMax[i]) {
+                waterLevel = leftMax[i];
+            }else {
+                waterLevel = rightMax[i];
+            }
+
+            int wateronBar = (waterLevel - height[i]);
+            trappedRainwater  += wateronBar;
         }
 
-        return totalWater;
+        return trappedRainwater;
     }
     public static void main(String args[]) {
-        int height[] = {4, 2, 0, 6, 3, 2, 5};
-        System.out.println("Total rainwater is = " + trappingRainWater(height));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Array size :");
+        int n = sc.nextInt();
+
+        int height[] = new int[n];
+        System.out.println("Enter Numbers :");
+        for(int i=0; i<height.length; i++) {
+            height[i] = sc.nextInt();
+        }
+
+        // //int height[] = {4, 2, 0, 6, 3, 2, 5};
+
+        int result = trappedRainwater(height);
+        System.out.println("Trapped Rain Water is  = " + result);
 
     }
 }
