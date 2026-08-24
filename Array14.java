56794aadd01b9da74294e4cf365461072aa6fe5e
Abhijeet Taywade <abhijeettaywade14@gmail.com>
2026-08-24 12:34:36 +0530

    Best time to buy and sell stock  Code

---
+++ Array14.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+// Bext time to buy and sell stock 
+public class Array14  { 
+    public static int buyAndSellStock(int prices[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int i=0; i<prices.length; i++) {
+            if(buyPrice < prices[i]) {
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            }else {
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String args[]) {
+        int prices[] = {7, 1, 5, 3, 6, 4};
+        int result = buyAndSellStock(prices);
+        System.out.println("Max Profit is = " + result);
+
+    }
+}
