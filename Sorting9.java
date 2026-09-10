e21ad5fc2d92f53e87c1272fc7bb37e81db30daf
Abhijeet Taywade <abhijeettaywade14@gmail.com>
2026-09-10 12:03:30 +0530

    add descending selection sort

--- Sorting9.java
+++ Sorting9.java
@@ -1,16 +1,19 @@
 import java.util.*;
 
-// Descending Bubble Sort
+// add descending selection sort "
 public class Sorting9 {
-    public static void bubbleSort(int arr[]) {
-        for(int turn=0; turn<arr.length-1; turn++) {
-            for(int j=0; j<arr.length-1-turn; j++) {
-                if(arr[j] < arr[j+1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+    public static void selectionSort(int arr[]) {
+        for(int i=0; i<arr.length-1; i++) {
+            int minPos = i;
+            for(int j=i+1; j<arr.length; j++) {
+                if(arr[minPos] < arr[j]) {
+                    minPos = j;
                 }
             }
+
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
         }
     }
 
@@ -20,8 +23,9 @@ public class Sorting9 {
         }
     }
     public static void main(String args[]) {
-        int arr[] = {4, 5, 1, 3, 2};
-        bubbleSort(arr);
+        int arr[] = {3,6,2,1,8,7,4,5,3,1};
+
+        selectionSort(arr);
         printArr(arr);
     }
 }
